b45447d9302b93c411d5bfcca7d8d81fb8282851
Lionel Brianto <lbrianto@be-bound.com>
2018-03-13 17:05:31 +0100

    Implement Configuration value object

---
+++ src/Configuration.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace BeBound\SDK;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+class Configuration
+{
+    private $beappName;
+    private $beappId;
+    private $beappVersion;
+    private $beappSecret;
+
+    private $logger;
+
+    public function __construct(string $beappName, int $beappId, int $beappVersion, string $beappSecret, ?LoggerInterface $logger = null)
+    {
+        $this->beappName = $beappName;
+        $this->beappId = $beappId;
+        $this->beappVersion = $beappVersion;
+        $this->beappSecret = $beappSecret;
+
+        $this->logger = $logger ?? new NullLogger();
+    }
+
+    public function getLogger(): LoggerInterface
+    {
+        return $this->logger;
+    }
+
+    public function getBeappName(): string
+    {
+        return $this->beappName;
+    }
+
+    public function getBeappId(): int
+    {
+        return $this->beappId;
+    }
+
+    public function getBeappVersion(): int
+    {
+        return $this->beappVersion;
+    }
+
+    public function getBeappSecret(): string
+    {
+        return $this->beappSecret;
+    }
+}
